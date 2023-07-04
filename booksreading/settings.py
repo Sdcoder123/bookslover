a91aca4a381725d2565fedb10efaf3c122769c1c
Sagnik Dutta <98347730+Sdcoder123@users.noreply.github.com>
2023-07-05 02:41:33 +0530

    Update settings.py

--- booksreading/settings.py
+++ booksreading/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-la**navrh5w(3kmymwievj(-+e81issrr!@=gez#h&nc&z3b6#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
